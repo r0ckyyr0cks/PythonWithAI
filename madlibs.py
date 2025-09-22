2dd4c95aaf7458a642ad94f60e2f21b82fad2040
rakesh.ranjan.qaengineer@gmail.com <rakesh.ranjan.qaengineer@gmail.com>
2025-09-22 11:52:34 +0530

    working files from Sep 2025

---
+++ madlibs.py
@@ -0,0 +1,15 @@
+name = "Neha"
+adjective = "red"
+favorite_snack = "Ice Cream"
+number = "15"
+type_of_tree = "Mango"
+
+silly_story = f"""
+    Once upon a time, there was a person named {name}.
+    Every day, {name} would wear a very {adjective} hat and eat {favorite_snack}.
+    One day, {name} decided to climb a {type_of_tree} tree that was {number} feet tall.
+    At the top of the tree, {name} found a magical land filled with talking animals and sparkling rivers.
+    And they all lived happily ever after!
+"""
+
+print(silly_story)
