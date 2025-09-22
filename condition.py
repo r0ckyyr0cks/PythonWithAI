0446784f97585ee12f569678a9521d29b789e156
rakesh.ranjan.qaengineer@gmail.com <rakesh.ranjan.qaengineer@gmail.com>
2025-09-22 22:02:38 +0530

    working files from Sep 2025

---
+++ condition.py
@@ -0,0 +1,5 @@
+a = 20
+if a % 2 == 0:
+    print(f"{a} is an even number")
+else:
+    print(f"{a} is an odd number")
---
+++ multipleCondition.py
@@ -0,0 +1,7 @@
+a = 3
+if a % 2 == 0 and a > 5:
+    print(f"{a} is an even number greater than 5")
+elif a % 2 == 0 and a <= 5:
+    print(f"{a} is an even number less than or equal to 5")
+else:
+    print(f"{a} is an odd number")
