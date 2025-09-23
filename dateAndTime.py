9b4829a418714f1065f0237e34c3fd8d6ebf9b0b
rakesh.ranjan.qaengineer@gmail.com <rakesh.ranjan.qaengineer@gmail.com>
2025-09-23 12:15:09 +0530

    working files from Sep 2025

---
+++ dateAndTime.py
@@ -0,0 +1,20 @@
+import datetime
+import pytz
+
+
+def get_current_datetime():
+    """Returns the current date and time as a formatted string."""
+    now = datetime.datetime.now()
+    return now.strftime("%d-%m-%Y %H:%M:%S")
+
+
+print(f"Local Timezone - {get_current_datetime()}")
+
+tz_NY = pytz.timezone('America/New_York')
+datetime_NY = datetime.datetime.now(tz_NY)
+print(f"New York Timezone - {datetime_NY.strftime('%d-%m-%Y %H:%M:%S')}")
+
+tz_London = pytz.timezone('Europe/London')
+datetime_London = datetime.datetime.now(tz_London)
+print(f"London Timezone - {datetime_London.strftime('%d-%m-%Y %H:%M:%S')}")
+
