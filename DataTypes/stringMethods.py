740501b1ac2b26006e5c775122d31da764d7fe5b
rakesh.ranjan.qaengineer@gmail.com <rakesh.ranjan.qaengineer@gmail.com>
2025-09-23 17:10:09 +0530

    working files from Sep 2025

---
+++ DataTypes/stringMethods.py
@@ -0,0 +1,32 @@
+# find - method returns the index of the first occurrence of the specified value
+txt = "Hello, welcome to my world."
+if txt.find("o") >= 0:
+    print("Yes, 'o' is present at index:", txt.find("o"))
+
+if txt.find("o", 5) >= 0:
+    print("Yes, another 'o' is present at index:", txt.find("o", 5))
+
+if txt.find("to") >= 0:
+    print("Yes, 'to' is present at index:", txt.find("to"))
+
+if txt.find("my", 10, 20) >= 0:
+    print("Yes, 'my' is present at index:", txt.find("my", 10, 20))
+
+if txt.rfind("o") >= 0:
+    print("Yes, last 'o' is present at index:", txt.rfind("o"))
+
+if txt.rfind("o", 0, 10) >= 0:
+    print("Yes, another last 'o' is present at index:", txt.rfind("o", 0, 10))
+
+print(txt.count("o"))  # count - method returns the number of occurrences of a substring in the given string
+print(txt.count("o", 5, 10))
+
+print(txt.startswith("Hello"))  # startswith - method returns True if the string starts with the specified value
+print(txt.startswith("welcome", 7, 20))
+
+print(txt.endswith("world."))  # endswith - method returns True if the string ends with the specified value
+print(txt.endswith("my", 5, 20))
+
+print(txt.upper())  # upper - method converts a string into upper case
+print(txt.lower())  # lower - method converts a string into lower case
+
