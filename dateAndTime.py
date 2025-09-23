5de3d28e6947a9efc882b180054faa7fc86b02b0
rakesh.ranjan.qaengineer@gmail.com <rakesh.ranjan.qaengineer@gmail.com>
2025-09-23 12:21:20 +0530

    working files from Sep 2025

--- dateAndTime.py
+++ dateAndTime.py
@@ -18,3 +18,17 @@ tz_London = pytz.timezone('Europe/London')
 datetime_London = datetime.datetime.now(tz_London)
 print(f"London Timezone - {datetime_London.strftime('%d-%m-%Y %H:%M:%S')}")
 
+
+print("^ " * 20)
+
+
+def get_datetime_in_timezone(timezone_str):
+    """Returns the current date and time in the specified timezone."""
+    tz = pytz.timezone(timezone_str)
+    datetime_tz = datetime.datetime.now(tz)
+    return datetime_tz.strftime("%d-%m-%Y %H:%M:%S")
+
+
+print(f"Tokyo Timezone - {get_datetime_in_timezone('Asia/Tokyo')}")
+print(f"Texas Timezone - {get_datetime_in_timezone('America/Chicago')}")
+
